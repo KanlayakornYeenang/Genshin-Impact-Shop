46bdea28c0f454aab125089078c598d90920d09a
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-10-24 19:54:20 +0700

    no message

--- src/data/ProductList.js
+++ src/data/ProductList.js
@@ -3,11 +3,11 @@ export const tops = [
         name:"[OFFICIAL MERCHANDISE] Genshin Impact Concert T-Shirt",
         price:30.99,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
         description:"Theme Print Material: 65% Cotton, 35% Polyester<br>\
         Shirt Material: 100% Cotton<br>\
@@ -36,59 +36,116 @@ export const tops = [
         No returns or exchanges are allowed for this product."
     },
     {
-        name:"",
-        price:null,
+        name:"[OFFICIAL MERCHANDISE] Genshin Impact Diluc T-Shirt",
+        price:30.99,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top2_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top2_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top2_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top2_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top2_5.png"}
+            "/images/Products/Apparel/Tops/Top2_1.png",
+            "/images/Products/Apparel/Tops/Top2_2.png",
+            "/images/Products/Apparel/Tops/Top2_3.png",
+            "/images/Products/Apparel/Tops/Top2_4.png",
+            "/images/Products/Apparel/Tops/Top2_5.png"
         ],
+        description:"Theme Print Material: 59% Cotton, 41% Polyester<br>\
+        Blank Material: 66% Cotton, 34% Polyester <br>\
+        <br>\
+        Size Estimate: Refer to the chart <br>\
+        <br>\
+        - - - - - - - -<br>\
+        Product Care:<br>\
+        <br>\
+        1. Washing machine, hand wash safe. DO NOT dry wash<br>\
+        <br>\
+        2. Wash at 30 degrees celcius, use PH neutral detergents<br>\
+        <br>\
+        3. No bleaching, dry in a cool and shady place<br>\
+        <br>\
+        4. Iron not hotter than 110 degrees celcius, cloth ironing is recommended, do not iron directly on top of the prints.<br>\
+        <br>\
+        5. Do not wash with other clothing in case of discoloration<br>\
+        <br>\
+        6. Prints may wear off after several washes. Please be gentle when scrubbing/washing the prints<br>\
+        <br>\
+        <<Product care translated from the flagship store's infographics>><br>\
+        <br>\
+        - - - - - - - -<br>\
+        No returns or exchanges are allowed for this product."
+    },
+    {
+        name:"[OFFICIAL MERCHANDISE] Genshin Impact Frolicking Flame Yoimiya T-shirt",
+        price:41.99,
+        images:[
+            "/images/Products/Apparel/Tops/Top3_1.png",
+            "/images/Products/Apparel/Tops/Top3_2.png",
+            "/images/Products/Apparel/Tops/Top3_3.png",
+            "/images/Products/Apparel/Tops/Top3_4.png",
+            "/images/Products/Apparel/Tops/Top3_5.png"
+        ],
+        description:"Shirt Material: 100% Cotton<br>\
+        <br>\
+        Size Estimate: Refer to the chart <br>\
+        <br>\
+        Sizes are taken from Flagship Store<br>\
+        <br>\
+        Do allow for discrepancies in measurement (up to 3cm difference).<br>\
+        <br>\
+        - - - - - - - -<br>\
+        Product Care:<br>\
+        <br>\
+        1. Please refer to the washing label before proceeding<br>\
+        <br>\
+        2. For washing both sides, please separate when washing the darker side<br>\
+        <br>\
+        3. No soaking of product<br>\
+        <br>\
+        4. Recommended to be washed before wearing<br>\
+        <br>\
+        5. As it contains prints, it's recommended to wash it inverted (With the prints inside) <br>\
+        <br>\
+        6. Do not apply high heat over printed areas <br>\
+        <br>\
+        7. Prints may wear off after several washes. Please be gentle when scrubbing/washing the prints<br>\
+        <br>\
+        8. The product may be a little furry after washing<br>\
+        <br>\
+        <<Product care translated from the flagship store's infographics>><br>\
+        - - - - - - - -<br>\
+        No returns or exchanges are allowed for this product."
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top3_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top3_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top3_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top3_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top3_5.png"}
+            "/images/Products/Apparel/Tops/Top4_1.png",
+            "/images/Products/Apparel/Tops/Top4_2.png",
+            "/images/Products/Apparel/Tops/Top4_3.png",
+            "/images/Products/Apparel/Tops/Top4_4.png",
+            "/images/Products/Apparel/Tops/Top4_5.png"
         ],
+        description:""
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top4_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top4_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top4_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top4_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top4_5.png"}
+            "/images/Products/Apparel/Tops/Top5_1.png",
+            "/images/Products/Apparel/Tops/Top5_2.png",
+            "/images/Products/Apparel/Tops/Top5_3.png",
+            "/images/Products/Apparel/Tops/Top5_4.png",
+            "/images/Products/Apparel/Tops/Top5_5.png"
         ],
+        description:""
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top5_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top5_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top5_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top5_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top5_5.png"}
+            "/images/Products/Apparel/Tops/Top6_1.png",
+            "/images/Products/Apparel/Tops/Top6_2.png",
+            "/images/Products/Apparel/Tops/Top6_3.png",
+            "/images/Products/Apparel/Tops/Top6_4.png",
+            "/images/Products/Apparel/Tops/Top6_5.png"
         ],
-    },
-    {
-        name:"",
-        price:null,
-        images:[
-            {url:"/images/Products/Apparel/Tops/Top6_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top6_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top6_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top6_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top6_5.png"}
-        ],
+        description:""
     }
 
 ];
@@ -98,23 +155,25 @@ export const bottom = [
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
+        description:""
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
+        description:""
     }
 ];
 
@@ -123,33 +182,36 @@ export const badges = [
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
+        description:""
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
+        description:""
     },
     {
         name:"",
         price:null,
         images:[
-            {url:"/images/Products/Apparel/Tops/Top1_1.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_2.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_3.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_4.png"},
-            {url:"/images/Products/Apparel/Tops/Top1_5.png"}
+            "/images/Products/Apparel/Tops/Top1_1.png",
+            "/images/Products/Apparel/Tops/Top1_2.png",
+            "/images/Products/Apparel/Tops/Top1_3.png",
+            "/images/Products/Apparel/Tops/Top1_4.png",
+            "/images/Products/Apparel/Tops/Top1_5.png"
         ],
+        description:""
     },
 ]
--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -51,13 +51,33 @@ export const collectibles = [
     url: "/collectibles/badges",
   },
   {
+    title: "Standees",
+    url: "/collectibles/standees",
+  },
+  {
+    title: "Pins",
+    url: "/collectibles/pins",
+  },
+  {
+    title: "Plush",
+    url: "/collectibles/plush",
+  },
+  {
+    title: "Figures",
+    url: "/collectibles/figures",
+  },
+  {
+    title: "Keychains",
+    url: "/collectibles/keychains",
+  },
+  {
     pictitle: [
-      "Xiao Series Long Sleeve Shirt",
-      "Genshin Impact Baal Themed T-shirt",
+      "Genshin Impact Taste of Summer Badges",
+      "Taroumaru Plushie",
     ],
     pic: [
-      "/images/products/apparel/tops/1.jpg",
-      "/images/products/apparel/tops/2.jpg",
+      "/images/products/collectibles/badges/Badge1_1.png",
+      "/images/products/collectibles/plush/plush3_1.png",
     ],
   },
 ];
@@ -73,12 +93,12 @@ export const accessories = [
   },
   {
     pictitle: [
-      "Xiao Series Long Sleeve Shirt",
-      "Genshin Impact Baal Themed T-shirt",
+      "Gorou's Shiba Inu Mug",
+      "Genshin Impact Themed Mouse Mat",
     ],
     pic: [
-      "/images/products/apparel/tops/1.jpg",
-      "/images/products/apparel/tops/2.jpg",
+      "/images/products/accessories/mugs/Mug1_1.png",
+      "/images/products/accessories/mousepads/MousePad2_1.png",
     ],
   },
 ];
