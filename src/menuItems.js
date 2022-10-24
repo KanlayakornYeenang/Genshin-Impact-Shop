666fcc842ce3613db256cddcef0a2e10da7eeb08
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-10-24 18:47:48 +0700

    no message

--- src/menuItems.js
+++ src/menuItems.js
@@ -28,12 +28,10 @@ export const apparel = [
   {
     title: "Tops",
     url: "/apparel/tops",
-    pic: "1.jpg"
   },
   {
     title: "Bottoms",
     url: "/apparel/bottoms",
-    pic: "./images/Products/Apparel/Tops/1.jpg"
   }
 ]
 
