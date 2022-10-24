270b5edd84709e0d034392daf94da0152d44402e
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-10-24 21:30:43 +0700

    no message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react"
 import Header from "../components/Header";
 import MyGallery from "../components/Slideshow";
 import ProductList from "../data/ProductList";
+import "../App.css";
 
 const Home = () => {
   return (
