ac2320e8be401798699530ba85d9ce38f2d066a7
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-10-24 21:11:17 +0700

    no message

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -12,12 +12,12 @@ const images = [
   {
     original:"/images/Slideshow/3.jpg"
   },
-  {
-    original:"/images/Slideshow/4.jpg"
-  },
-  {
-    original:"/images/Slideshow/5.jpg"
-  },
+  // {
+  //   original:"/images/Slideshow/4.jpg"
+  // },
+  // {
+  //   original:"/images/Slideshow/5.jpg"
+  // },
 ];
 
 class MyGallery extends React.Component {
