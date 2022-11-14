9356233c384cc60484ac6418c6b237a255c34d8f
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-11-14 17:01:25 +0700

    no message

--- src/components/SizeChart.js
+++ src/components/SizeChart.js
@@ -1,32 +1,52 @@
+import React, { useState } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import { borderRadius } from "@mui/system";
 
 const SizeChart = () => {
+  const [size, setSize] = useState()
+  const handleSizeChange = (e, newValue) => {
+    setSize(newValue);
+  };
   const StyleToggleButton = {
     color: "white",
     outline: "white solid 0.1vw",
     fontSize: "0.75vw",
     width: "15%",
     padding: "0.25vw 0vw",
-    borderRadius: "0"
+    borderRadius: "0",
   };
   return (
-    <div style={{display:"flex", flexFlow:"column", rowGap:"1vw"}}>
-      <div style={{fontSize:"1vw"}}>SIZE</div>
+    <div style={{ display: "flex", flexFlow: "column", rowGap: "1vw" }}>
+      <div style={{ fontSize: "1vw" }}>SIZE</div>
       <ToggleButtonGroup
+      color="primary"
+        value={size}
+        exclusive
+        onChange={handleSizeChange}
         sx={{
           display: "flex",
           flexFlow: "wrap",
           gap: "1vw",
         }}
       >
-        <ToggleButton sx={StyleToggleButton}>XS</ToggleButton>
-        <ToggleButton sx={StyleToggleButton}>S</ToggleButton>
-        <ToggleButton sx={StyleToggleButton}>M</ToggleButton>
-        <ToggleButton sx={StyleToggleButton}>L</ToggleButton>
-        <ToggleButton sx={StyleToggleButton}>XL</ToggleButton>
-        <ToggleButton sx={StyleToggleButton}>2XL</ToggleButton>
+        <ToggleButton value="xs" sx={StyleToggleButton}>
+          XS
+        </ToggleButton>
+        <ToggleButton value="s" sx={StyleToggleButton}>
+          S
+        </ToggleButton>
+        <ToggleButton value="m" sx={StyleToggleButton}>
+          M
+        </ToggleButton>
+        <ToggleButton value="l" sx={StyleToggleButton}>
+          L
+        </ToggleButton>
+        <ToggleButton value="xl" sx={StyleToggleButton}>
+          XL
+        </ToggleButton>
+        <ToggleButton value="2xl" sx={StyleToggleButton}>
+          2XL
+        </ToggleButton>
       </ToggleButtonGroup>
     </div>
   );
