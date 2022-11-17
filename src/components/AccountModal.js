c46b166957830e91127a6e95a36e2c926d000b44
ilovemhuukrob <kanlayakorn.yeenang@gmail.com>
2022-11-17 17:28:06 +0700

    no message

--- src/components/AccountModal.js
+++ src/components/AccountModal.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tabs } from "./Navbar";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import "./AccountModal.css"
-import SignIn from "./ContentModal";
+import "./AccountModal.css";
+import SignIn, { Email, Username, Password } from "./ContentModal";
 
-const style = {
+const StyledBox = {
   position: "absolute",
   top: "50%",
   left: "50%",
@@ -17,7 +17,29 @@ const style = {
 const AccountModal = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setOpen(false);
+    setPopup("SignIn");
+  };
+
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [popup, setPopup] = React.useState("SignIn");
+  const handleClick = (popupState, submit) => {
+    setPopup(popupState);
+    if (submit != "Nothing") {
+      if (popupState == "Username") {
+        localStorage.setItem("Email", submit);
+      } else if (popupState == "Password") {
+        localStorage.setItem("Username", submit);
+      } else if (popupState == "Done") {
+        localStorage.setItem("Password", submit);
+      }
+    }
+    console.log(submit);
+  };
 
   return (
     <div>
@@ -30,10 +52,43 @@ const AccountModal = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box sx={StyledBox}>
           <div className="modal-container">
             <div className="modal-wrapper">
-              <SignIn />
+              {(() => {
+                switch (popup) {
+                  case "SignIn":
+                    return <SignIn handleClick={handleClick} />;
+                  case "Email":
+                    return (
+                      <Email
+                        handleClick={handleClick}
+                        setEmail={setEmail}
+                        email={email}
+                      />
+                    );
+                  case "Username":
+                    return (
+                      <Username
+                        handleClick={handleClick}
+                        setUsername={setUsername}
+                        username={username}
+                      />
+                    );
+                  case "Password":
+                    return (
+                      <Password
+                        handleClick={handleClick}
+                        setPassword={setPassword}
+                        password={password}
+                      />
+                    );
+                  case "Done":
+                    return <SignIn handleClick={handleClick} />;
+                  default:
+                    return null;
+                }
+              })()}
             </div>
           </div>
         </Box>
--- src/components/ContentModal.js
+++ src/components/ContentModal.js
@@ -10,17 +10,34 @@ const StyledOutlineInput = styled(OutlinedInput)({
   },
 });
 
-const SignIn = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordShown, setPasswordShown] = useState(false);
-  const togglePassword = () => {
-    setPasswordShown(!passwordShown);
-  };
-
+export const Password = ({ handleClick, setPassword, password }) => {
   return (
     <div className="modal-content">
-      <h1>Sign In</h1>
+      <h1 style={{ textAlign: "center" }}>
+        Choose
+        <br />a Password
+      </h1>
+      <h2 style={{ fontWeight: "100", color: "#7a7a7a", fontSize: "1.25vw" }}>
+        Make sure it's a good one.
+      </h2>
+      <PasswordField label="PASSWORD" />
+      <PasswordField label="CONFIRM PASSWORD" />
+      <div className="arrowicon">
+        <p>
+          <HiArrowRight onClick={() => handleClick("Done", password)} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export const Username = ({ handleClick, setUsername, username }) => {
+  return (
+    <div className="modal-content">
+      <h1>Choose a Username</h1>
+      <h2 style={{ fontWeight: "100", color: "#7a7a7a", fontSize: "1.25vw" }}>
+        Used for sign in to game.
+      </h2>
       <FormControl>
         <InputLabel variant="filled" className="inputlabel">
           USERNAME
@@ -30,27 +47,84 @@ const SignIn = () => {
           onChange={(e) => setUsername(e.target.value)}
         />
       </FormControl>
-      <FormControl className="password-wrapper">
+      <div className="arrowicon">
+        <p>
+          <HiArrowRight onClick={() => handleClick("Password", username)} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export const Email = ({ handleClick, setEmail, email }) => {
+  return (
+    <div className="modal-content">
+      <h1>What's your Email?</h1>
+      <FormControl>
         <InputLabel variant="filled" className="inputlabel">
-          PASSWORD
+          Email
         </InputLabel>
-        <StyledOutlineInput
-          type={passwordShown ? "text" : "password"}
-          className="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+        <OutlinedInput
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <button onClick={togglePassword}>
-          {passwordShown ? <AiFillEye /> : <AiFillEyeInvisible />}
-        </button>
       </FormControl>
+      <div className="arrowicon">
+        <p>
+          <HiArrowRight onClick={() => handleClick("Username", email)} />
+        </p>
+      </div>
+      <p className="modal-footer" onClick={() => handleClick("SignIn")}>
+        ALREADY HAVE AN ACCOUNT?
+      </p>
+    </div>
+  );
+};
+
+const SignIn = ({ handleClick }) => {
+  return (
+    <div className="modal-content">
+      <h1>Sign In</h1>
+      <FormControl>
+        <InputLabel variant="filled" className="inputlabel">
+          USERNAME
+        </InputLabel>
+        <OutlinedInput />
+      </FormControl>
+      <PasswordField label="PASSWORD" />
       <div className="arrowicon">
         <p>
           <HiArrowRight />
         </p>
       </div>
-      <p className="modal-footer">CREATE ACCOUNT</p>
+      <p
+        className="modal-footer"
+        onClick={() => handleClick("Email", "Nothing")}
+      >
+        CREATE ACCOUNT
+      </p>
     </div>
+  );
+};
+
+const PasswordField = (props) => {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+  return (
+    <FormControl className="password-wrapper">
+      <InputLabel variant="filled" className="inputlabel">
+        {props.label}
+      </InputLabel>
+      <StyledOutlineInput
+        type={passwordShown ? "text" : "password"}
+        className="password"
+      />
+      <button onClick={togglePassword}>
+        {passwordShown ? <AiFillEye /> : <AiFillEyeInvisible />}
+      </button>
+    </FormControl>
   );
 };
 
